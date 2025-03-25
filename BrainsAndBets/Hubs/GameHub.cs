f0040f52238063c42b9a5e97c4c43caabef7fc98
Carson Hatch <carson.hatch@churchofjesuschrist.org>
2025-03-25 12:57:20 -0600

    init

---
+++ BrainsAndBets/Hubs/GameHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BrainsAndBets.Hubs
+{
+    public class GameHub : Hub
+    {
+        public async Task JoinGame(string user)
+        {
+            await Clients.All.SendAsync("JoinedGame", user);
+        }
+    }
+}
